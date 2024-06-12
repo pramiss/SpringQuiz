1bf61fb7edb663c0aa08205c2eeec359805fbecc
DESKTOP-K3GM7KB\user <test@test.com>
2024-06-12 18:46:22 +0900

    0612 수업내용

--- src/main/java/com/quiz/lesson01/Quiz03Controller.java
+++ src/main/java/com/quiz/lesson01/Quiz03Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller // View와 연결할 것
 public class Quiz03Controller {
-	
+		
 	@RequestMapping("/lesson01/quiz03")
 	public String quiz03() {
 		return "lesson01/quiz03";
