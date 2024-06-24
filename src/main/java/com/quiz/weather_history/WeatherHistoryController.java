fda72a71a95824649b8bc117f328884b80f621fb
Jinha <kkang56508@naver.com>
2024-06-25 00:49:27 +0900

    lesson05 quiz04 마크업 대충

--- src/main/java/com/quiz/weather_history/WeatherHistoryController.java
+++ src/main/java/com/quiz/weather_history/WeatherHistoryController.java
@@ -1,13 +1,20 @@
 package com.quiz.weather_history;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/weather-history")
 @Controller
 public class WeatherHistoryController {
 
-	@GetMapping("weather-list-view")
+	@GetMapping("/weather-list-view")
+	public String weatherListView() {
+		return "weather_history/weatherList";
+	}
 	
-	@GetMapping("add-weather-view")
+	@GetMapping("/add-weather-view")
+	public String addWeatherView() {
+		return "weather_history/addWeather";
+	}
 }
