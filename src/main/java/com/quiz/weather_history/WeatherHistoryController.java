79d410f4fdb858941ad5bad50f0d4ea663ad4879
DESKTOP-K3GM7KB\user <test@test.com>
2024-06-24 18:51:21 +0900

    0624, spring 5.thymeleaf quiz04,숙제:lesson05 quiz04최대한 마크업

---
+++ src/main/java/com/quiz/weather_history/WeatherHistoryController.java
@@ -0,0 +1,13 @@
+package com.quiz.weather_history;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/weather-history")
+@Controller
+public class WeatherHistoryController {
+
+	@GetMapping("weather-list-view")
+	
+	@GetMapping("add-weather-view")
+}
