7ed073989b89b2cc7b17f9c7666f12b504f2235d
Jinha <kkang56508@naver.com>
2024-07-04 00:07:50 +0900

    JPA quiz1

--- src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
+++ src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
@@ -29,6 +29,25 @@ public class Lesson07Quiz02RestController {
 		return recruitRepository.findByCompanyId(companyId);
 	}
 	
+	@GetMapping("/3")
+	public List<RecruitEntity> select3() {
+		return recruitRepository.findByPositionAndType("웹 back-end 개발자", "정규직");
+	}
+	
+	@GetMapping("/4")
+	public List<RecruitEntity> select4() {
+		return recruitRepository.findByTypeOrSalaryGreaterThan("정규직", 9000);
+	}
+	
+	@GetMapping("/5")
+	public List<RecruitEntity> select5() {
+		return recruitRepository.findTop3ByOrderBySalaryDesc();
+	}
+	
+	@GetMapping("/6")
+	public List<RecruitEntity> select6() {
+		return recruitRepository.findByRegionAndSalaryBetween("성남시 분당구", 7000, 8500);
+	}
 	
 	
 	
--- src/main/java/com/quiz/lesson07/repository/RecruitRepository.java
+++ src/main/java/com/quiz/lesson07/repository/RecruitRepository.java
@@ -8,4 +8,8 @@ import com.quiz.lesson07.entity.RecruitEntity;
 
 public interface RecruitRepository extends JpaRepository<RecruitEntity, Integer> {
 	public List<RecruitEntity> findByCompanyId(int companyId);
+	public List<RecruitEntity> findByPositionAndType(String position, String type);
+	public List<RecruitEntity> findByTypeOrSalaryGreaterThan(String type, int salary);
+	public List<RecruitEntity> findTop3ByOrderBySalaryDesc();
+	public List<RecruitEntity> findByRegionAndSalaryBetween(String region, int startSalary, int endSalary);	
 }
